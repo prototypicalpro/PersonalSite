0461d7604154f2f96e80132111adbc75da938d9b
Noah Laptop <noah@koontzs.com>
2019-06-22 19:20:29 -0700

    Updates

---
+++ precache-manifest.81e0dedb06d20f83ff143ab3cea20813.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c875956f359c5a570b2c036686da866",
+    "url": "/PrototypicalPro/index.html"
+  },
+  {
+    "revision": "9c4e1405939d9303382f",
+    "url": "/PrototypicalPro/static/js/2.8f409db9.chunk.js"
+  },
+  {
+    "revision": "4bacf9384306230d18b4",
+    "url": "/PrototypicalPro/static/js/main.5282d7fc.chunk.js"
+  },
+  {
+    "revision": "23d099609dcc84e13d7e",
+    "url": "/PrototypicalPro/static/js/runtime~main.e29a380b.js"
+  },
+  {
+    "revision": "5c13b865ae9da7ab5ce1747c6b8360a6",
+    "url": "/PrototypicalPro/static/media/LDR_RGB1_0.5c13b865.png"
+  },
+  {
+    "revision": "f3f731a5bc865c0f30b71ce91e296cbe",
+    "url": "/PrototypicalPro/static/media/image.f3f731a5.PNG"
+  },
+  {
+    "revision": "632a3a6bae4896acf0fd0e1778f911e1",
+    "url": "/PrototypicalPro/static/media/proto.632a3a6b.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/PrototypicalPro/static/media/react_logo.5d5d9eef.svg"
+  }
+]);
