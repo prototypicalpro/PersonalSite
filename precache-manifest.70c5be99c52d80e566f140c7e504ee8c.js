77765d00145051e84775d43d697525f508c6ee5b
Noah Laptop <noah@koontzs.com>
2019-07-14 18:34:00 -0700

    Updates

---
+++ precache-manifest.70c5be99c52d80e566f140c7e504ee8c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "905283ae710c311637fd46a381544cdf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e88c3139e582c7a59072",
+    "url": "/static/js/2.bf6c10c7.chunk.js"
+  },
+  {
+    "revision": "d1cbfdbaa6497a2cbd4c",
+    "url": "/static/js/main.3fedbb18.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a99c0b3c03bc33930e1bc24a984efd12",
+    "url": "/static/media/backthumb.a99c0b3c.jpg"
+  },
+  {
+    "revision": "8b8bbaafa01fec2f3ce6b27e7f56b0d0",
+    "url": "/static/media/backvid.8b8bbaaf.webm"
+  },
+  {
+    "revision": "2f21dbfc8553eac7bb796e99311812e6",
+    "url": "/static/media/cloud.2f21dbfc.svg"
+  },
+  {
+    "revision": "f486b0eb823be0185b36278b13286427",
+    "url": "/static/media/down.f486b0eb.svg"
+  },
+  {
+    "revision": "0da036c25a7e5a08608770a9785a8f24",
+    "url": "/static/media/email.0da036c2.svg"
+  },
+  {
+    "revision": "2fee49af3caa56b32bf36b7d4fc1139c",
+    "url": "/static/media/embedded.2fee49af.svg"
+  },
+  {
+    "revision": "8a1ba32d3c7b69b57d05b74cd65d8bd8",
+    "url": "/static/media/github.8a1ba32d.svg"
+  },
+  {
+    "revision": "8dc04bcddc1ee128393621e994aa53e9",
+    "url": "/static/media/linkedin.8dc04bcd.svg"
+  },
+  {
+    "revision": "a2192e920e093de575935b908213b8fb",
+    "url": "/static/media/proto.a2192e92.svg"
+  },
+  {
+    "revision": "b252b5a246205c848dd774b49007e929",
+    "url": "/static/media/web.b252b5a2.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -13,46 +13,27 @@
 
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
+importScripts(
+  "/precache-manifest.70c5be99c52d80e566f140c7e504ee8c.js"
+);
+
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'SKIP_WAITING') {
     self.skipWaiting();
   }
 });
 
+workbox.core.clientsClaim();
+
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
  * requests for URLs in the manifest.
  * See https://goo.gl/S9QRab
  */
-self.__precacheManifest = [
-  {
-    "url": "/static/js/2.4ea03617.chunk.js"
-  },
-  {
-    "url": "/static/js/main.e1c63b89.chunk.js"
-  },
-  {
-    "url": "/static/js/runtime~main.a8a9905a.js"
-  },
-  {
-    "url": "/200.html",
-    "revision": "3460b381e76a34326ce2c206e790e744"
-  },
-  {
-    "url": "/index.html",
-    "revision": "6ede4559b03c5dca619f1d04b9b3a525"
-  },
-  {
-    "url": "/static/media/backthumb.a99c0b3c.jpg"
-  }
-].concat(self.__precacheManifest || []);
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/200.html"), {
-  whitelist: [/^(?!\/__).*/],
-  blacklist: [/^\/sharethumb\.png/],
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
-
-workbox.routing.registerRoute(/^https:\/\/fonts\.googleapis\.com/, new workbox.strategies.StaleWhileRevalidate({ "cacheName":"google-fonts-initial", plugins: [] }), 'GET');
-workbox.routing.registerRoute(/^https:\/\/fonts\.gstatic\.com/, new workbox.strategies.StaleWhileRevalidate({ "cacheName":"google-fonts-webfonts", plugins: [] }), 'GET');
-workbox.routing.registerRoute(/^https:\/\/api\.prototypical\.pro/, new workbox.strategies.NetworkFirst({ "cacheName":"api-cache", plugins: [] }), 'GET');
